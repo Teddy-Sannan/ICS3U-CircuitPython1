c9de22f8664c0ecd20d2a5ab47bb1a4610717bf0
Ubuntu <ubuntu@ip-172-31-16-204.ec2.internal>
2019-09-17 17:22:59 +0000

    Hello World on pybadge

---
+++ hello_world.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Created by: Teddy Sannan
+# Created on: September 2019
+# This program is the "Hello, World!" program on the PyBadge
+
+
+def main():
+    # this function prints to the screen and console
+    print("\n\n\n")
+    print("Hello, World!")
+
+    while True:
+        # repeat forever, or you turn it off!
+        pass
+
+
+if __name__ == "__main__":
+    main()
